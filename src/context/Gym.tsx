9004f4ab5a684b8a7d5d362c05a381a0541d949d
Realsteel <singhrohan.1996@gmail.com>
2024-09-30 02:25:36 +0530

    added logo & token check

--- src/context/Gym.tsx
+++ src/context/Gym.tsx
@@ -1,24 +1,48 @@
-import React, { createContext, useContext, ReactNode } from 'react';
-import { useParams } from 'react-router-dom';
-import { useGyms } from '@/hooks/';
+import React, {
+  createContext,
+  useContext,
+  ReactNode,
+  useState,
+  useEffect,
+} from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useGyms } from "@/hooks/";
 
 interface GymNameContextProps {
   gymName: string;
   loading: boolean;
+  isAuthenticated: boolean;
 }
 
-const GymNameContext = createContext<GymNameContextProps>({ gymName: '', loading: true });
+const GymNameContext = createContext<GymNameContextProps>({
+  gymName: "",
+  loading: true,
+  isAuthenticated: false,
+});
 
-export const GymNameProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+export const GymNameProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
   const { gymId } = useParams<{ gymId: string }>();
   const { gyms, loading } = useGyms();
+  const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
-  const gym = gyms.find(gym => gym.id === gymId);
-  const gymName = gym ? gym.name : 'STEEL';
-  console.log(gymId, gymName)
+  const gym = gyms.find((gym) => gym.id === gymId);
+  const gymName = gym ? gym.name : "STEEL";
+  console.log(gymId, gymName);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/");
+    } else {
+      setIsAuthenticated(true);
+    }
+  }, [navigate]);
 
   return (
-    <GymNameContext.Provider value={{ gymName, loading }}>
+    <GymNameContext.Provider value={{ gymName, loading, isAuthenticated }}>
       {children}
     </GymNameContext.Provider>
   );
