055849baf634ecc88a197aa0801a6d7f249b7c6e
Realsteel <singhrohan.1996@gmail.com>
2024-10-02 23:43:14 +0530

    manifest fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,39 @@
-const CACHE_NAME = 'app-cache-v1';
+const CACHE_NAME = "app-cache-v1";
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/Admin-Logo.png',
-  '/vite.svg',
+  "/",
+  "/index.html",
+  "/Admin-Logo.png",
+  "/vite.svg",
   // Add other static assets like CSS, JS files if needed
 ];
+let deferredPrompt;
 
-self.addEventListener('install', (event) => {
+window.addEventListener("beforeinstallprompt", (e) => {
+  // Prevent the mini info bar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later
+  deferredPrompt = e;
+
+  // Show your custom install button here
+  const installButton = document.getElementById("installButton");
+  installButton.style.display = "block";
+
+  installButton.addEventListener("click", () => {
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPrompt = null; // Clear the deferredPrompt variable
+    });
+  });
+});
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
@@ -15,7 +41,7 @@ self.addEventListener('install', (event) => {
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
