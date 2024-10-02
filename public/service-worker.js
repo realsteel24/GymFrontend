4c3c58ea6ab21c99867c75308256c1b429862ea5
Realsteel <singhrohan.1996@gmail.com>
2024-10-02 23:49:32 +0530

    manifest fixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,30 +7,41 @@ const urlsToCache = [
   // Add other static assets like CSS, JS files if needed
 ];
 let deferredPrompt;
+let installButtonVisible = false; // Flag to track visibility
 
 window.addEventListener("beforeinstallprompt", (e) => {
-  // Prevent the mini info bar from appearing on mobile
+  // Prevent the default mini-info bar from appearing
   e.preventDefault();
   // Stash the event so it can be triggered later
   deferredPrompt = e;
 
-  // Show your custom install button here
-  const installButton = document.getElementById("installButton");
-  installButton.style.display = "block";
+  // Set a timeout to show the prompt after 30 seconds
+  setTimeout(() => {
+    if (deferredPrompt) {
+      // Only show the prompt if the button is not already shown
+      if (!installButtonVisible) {
+        // Show your custom install button here
+        const installButton = document.getElementById("installButton");
+        installButton.style.display = "block";
+        installButtonVisible = true; // Update flag
 
-  installButton.addEventListener("click", () => {
-    // Show the install prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
-      } else {
-        console.log("User dismissed the A2HS prompt");
+        installButton.addEventListener("click", () => {
+          // Show the install prompt
+          deferredPrompt.prompt();
+          // Wait for the user to respond to the prompt
+          deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === "accepted") {
+              console.log("User accepted the A2HS prompt");
+            } else {
+              console.log("User dismissed the A2HS prompt");
+            }
+            deferredPrompt = null; // Clear the deferredPrompt variable
+            installButton.style.display = "none"; // Hide the button after prompt
+          });
+        });
       }
-      deferredPrompt = null; // Clear the deferredPrompt variable
-    });
-  });
+    }
+  }, 30000); // 30 seconds
 });
 
 self.addEventListener("install", (event) => {
